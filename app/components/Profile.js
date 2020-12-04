144cb74f3390caa3202ccc0ac864152a17659e1e
212562268-Cherian,Shejoy <212562268@ge.com>
2020-12-04 10:49:47 +0530

    end of posts

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useContext } from 'react'
-import Page from './Page'
-import { useParams, NavLink, Switch, Route } from 'react-router-dom'
-import Axios from 'axios'
-import StateContext from '../StateContext'
-import ProfilePosts from './ProfilePosts'
-import ProfileFollow from './ProfileFollow'
-import { useImmer } from 'use-immer'
+import React, { useEffect, useContext } from "react"
+import Page from "./Page"
+import { useParams, NavLink, Switch, Route } from "react-router-dom"
+import Axios from "axios"
+import StateContext from "../StateContext"
+import ProfilePosts from "./ProfilePosts"
+import ProfileFollowers from "./ProfileFollowers"
+import ProfileFollowing from "./ProfileFollowing"
+import { useImmer } from "use-immer"
 
 function Profile() {
   const { username } = useParams()
@@ -15,11 +16,11 @@ function Profile() {
     startFollowingRequestCount: 0,
     stopFollowingRequestCount: 0,
     profileData: {
-      profileUsername: '...',
-      profileAvatar: 'https://gravatar.com/avatar/placeholder?s=128',
+      profileUsername: "...",
+      profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
       isFollowing: false,
-      counts: { postCount: '', followerCount: '', followingCount: '' },
-    },
+      counts: { postCount: "", followerCount: "", followingCount: "" }
+    }
   })
 
   useEffect(() => {
@@ -27,16 +28,12 @@ function Profile() {
 
     async function fetchData() {
       try {
-        const response = await Axios.post(
-          `/profile/${username}`,
-          { token: appState.user.token },
-          { cancelToken: ourRequest.token }
-        )
-        setState((draft) => {
+        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+        setState(draft => {
           draft.profileData = response.data
         })
       } catch (e) {
-        console.log('There was a problem.')
+        console.log("There was a problem.")
       }
     }
     fetchData()
@@ -47,7 +44,7 @@ function Profile() {
 
   useEffect(() => {
     if (state.startFollowingRequestCount) {
-      setState((draft) => {
+      setState(draft => {
         draft.followActionLoading = true
       })
 
@@ -55,18 +52,14 @@ function Profile() {
 
       async function fetchData() {
         try {
-          const response = await Axios.post(
-            `/addFollow/${state.profileData.profileUsername}`,
-            { token: appState.user.token },
-            { cancelToken: ourRequest.token }
-          )
-          setState((draft) => {
+          const response = await Axios.post(`/addFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
             draft.profileData.isFollowing = true
             draft.profileData.counts.followerCount++
             draft.followActionLoading = false
           })
         } catch (e) {
-          console.log('There was a problem.')
+          console.log("There was a problem.")
         }
       }
       fetchData()
@@ -78,7 +71,7 @@ function Profile() {
 
   useEffect(() => {
     if (state.stopFollowingRequestCount) {
-      setState((draft) => {
+      setState(draft => {
         draft.followActionLoading = true
       })
 
@@ -86,18 +79,14 @@ function Profile() {
 
       async function fetchData() {
         try {
-          const response = await Axios.post(
-            `/removeFollow/${state.profileData.profileUsername}`,
-            { token: appState.user.token },
-            { cancelToken: ourRequest.token }
-          )
-          setState((draft) => {
+          const response = await Axios.post(`/removeFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
             draft.profileData.isFollowing = false
             draft.profileData.counts.followerCount--
             draft.followActionLoading = false
           })
         } catch (e) {
-          console.log('There was a problem.')
+          console.log("There was a problem.")
         }
       }
       fetchData()
@@ -108,79 +97,54 @@ function Profile() {
   }, [state.stopFollowingRequestCount])
 
   function startFollowing() {
-    setState((draft) => {
+    setState(draft => {
       draft.startFollowingRequestCount++
     })
   }
 
   function stopFollowing() {
-    setState((draft) => {
+    setState(draft => {
       draft.stopFollowingRequestCount++
     })
   }
 
   return (
-    <Page title='Profile Screen'>
+    <Page title="Profile Screen">
       <h2>
-        <img className='avatar-small' src={state.profileData.profileAvatar} />{' '}
-        {state.profileData.profileUsername}
-        {appState.loggedIn &&
-          !state.profileData.isFollowing &&
-          appState.user.username != state.profileData.profileUsername &&
-          state.profileData.profileUsername != '...' && (
-            <button
-              onClick={startFollowing}
-              disabled={state.followActionLoading}
-              className='btn btn-primary btn-sm ml-2'
-            >
-              Follow <i className='fas fa-user-plus'></i>
-            </button>
-          )}
-        {appState.loggedIn &&
-          state.profileData.isFollowing &&
-          appState.user.username != state.profileData.profileUsername &&
-          state.profileData.profileUsername != '...' && (
-            <button
-              onClick={stopFollowing}
-              disabled={state.followActionLoading}
-              className='btn btn-danger btn-sm ml-2'
-            >
-              Stop Following <i className='fas fa-user-times'></i>
-            </button>
-          )}
+        <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
+        {appState.loggedIn && !state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+          <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
+            Follow <i className="fas fa-user-plus"></i>
+          </button>
+        )}
+        {appState.loggedIn && state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+          <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
+            Stop Following <i className="fas fa-user-times"></i>
+          </button>
+        )}
       </h2>
 
-      <div className='profile-nav nav nav-tabs pt-2 mb-4'>
-        <NavLink
-          exact
-          to={`/profile/${state.profileData.profileUsername}`}
-          className='nav-item nav-link'
-        >
+      <div className="profile-nav nav nav-tabs pt-2 mb-4">
+        <NavLink exact to={`/profile/${state.profileData.profileUsername}`} className="nav-item nav-link">
           Posts: {state.profileData.counts.postCount}
         </NavLink>
-        <NavLink
-          to={`/profile/${state.profileData.profileUsername}/followers`}
-          className='nav-item nav-link'
-        >
+        <NavLink to={`/profile/${state.profileData.profileUsername}/followers`} className="nav-item nav-link">
           Followers: {state.profileData.counts.followerCount}
         </NavLink>
-        <NavLink
-          to={`/profile/${state.profileData.profileUsername}/following`}
-          className='nav-item nav-link'
-        >
+        <NavLink to={`/profile/${state.profileData.profileUsername}/following`} className="nav-item nav-link">
           Following: {state.profileData.counts.followingCount}
         </NavLink>
       </div>
 
       <Switch>
-        <Route exact path='/profile/:username'>
+        <Route exact path="/profile/:username">
           <ProfilePosts />
         </Route>
-        <Route path='/profile/:username/followers'>
-          <ProfileFollow action='followers' />
+        <Route path="/profile/:username/followers">
+          <ProfileFollowers />
         </Route>
-        <Route path='/profile/:username/following'>
-          <ProfileFollow action='following' />
+        <Route path="/profile/:username/following">
+          <ProfileFollowing />
         </Route>
       </Switch>
     </Page>
