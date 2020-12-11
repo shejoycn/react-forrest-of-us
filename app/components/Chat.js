71a6bf487a1abda2b797a09cc9813f86bf794146
212562268-Cherian,Shejoy <212562268@ge.com>
2020-12-11 13:10:13 +0530

    changes

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useContext, useRef } from 'react'
-import StateContext from '../StateContext'
-import DispatchContext from '../DispatchContext'
-import { useImmer } from 'use-immer'
-import { Link } from 'react-router-dom'
-import io from 'socket.io-client'
-const socket = io('http://localhost:8080')
+import React, { useEffect, useContext, useRef } from "react"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
+import { useImmer } from "use-immer"
+import { Link } from "react-router-dom"
+import io from "socket.io-client"
+const socket = io("http://localhost:8080")
 
 function Chat() {
   const chatField = useRef(null)
@@ -12,19 +12,19 @@ function Chat() {
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
   const [state, setState] = useImmer({
-    fieldValue: '',
+    fieldValue: "",
     chatMessages: [],
   })
 
   useEffect(() => {
     if (appState.isChatOpen) {
       chatField.current.focus()
-      appDispatch({ type: 'clearUnreadChatCount' })
+      appDispatch({ type: "clearUnreadChatCount" })
     }
   }, [appState.isChatOpen])
 
   useEffect(() => {
-    socket.on('chatFromServer', (message) => {
+    socket.on("chatFromServer", (message) => {
       setState((draft) => {
         draft.chatMessages.push(message)
       })
@@ -34,7 +34,7 @@ function Chat() {
   useEffect(() => {
     chatLog.current.scrollTop = chatLog.current.scrollHeight
     if (state.chatMessages.length && !appState.isChatOpen) {
-      appDispatch({ type: 'incrementUnreadChatCount' })
+      appDispatch({ type: "incrementUnreadChatCount" })
     }
   }, [state.chatMessages])
 
@@ -48,59 +48,43 @@ function Chat() {
   function handleSubmit(e) {
     e.preventDefault()
     // Send message to chat server
-    socket.emit('chatFromBrowser', {
-      message: state.fieldValue,
-      token: appState.user.token,
-    })
+    socket.emit("chatFromBrowser", { message: state.fieldValue, token: appState.user.token })
 
     setState((draft) => {
       // Add message to state collection of messages
-      draft.chatMessages.push({
-        message: draft.fieldValue,
-        username: appState.user.username,
-        avatar: appState.user.avatar,
-      })
-      draft.fieldValue = ''
+      draft.chatMessages.push({ message: draft.fieldValue, username: appState.user.username, avatar: appState.user.avatar })
+      draft.fieldValue = ""
     })
   }
 
   return (
-    <div
-      id='chat-wrapper'
-      className={
-        'chat-wrapper shadow border-top border-left border-right ' +
-        (appState.isChatOpen ? 'chat-wrapper--is-visible' : '')
-      }
-    >
-      <div className='chat-title-bar bg-primary'>
+    <div id="chat-wrapper" className={"chat-wrapper shadow border-top border-left border-right " + (appState.isChatOpen ? "chat-wrapper--is-visible" : "")}>
+      <div className="chat-title-bar bg-primary">
         Chat
-        <span
-          onClick={() => appDispatch({ type: 'closeChat' })}
-          className='chat-title-bar-close'
-        >
-          <i className='fas fa-times-circle'></i>
+        <span onClick={() => appDispatch({ type: "closeChat" })} className="chat-title-bar-close">
+          <i className="fas fa-times-circle"></i>
         </span>
       </div>
-      <div id='chat' className='chat-log' ref={chatLog}>
+      <div id="chat" className="chat-log" ref={chatLog}>
         {state.chatMessages.map((message, index) => {
           if (message.username == appState.user.username) {
             return (
-              <div key={index} className='chat-self'>
-                <div className='chat-message'>
-                  <div className='chat-message-inner'>{message.message}</div>
+              <div key={index} className="chat-self">
+                <div className="chat-message">
+                  <div className="chat-message-inner">{message.message}</div>
                 </div>
-                <img className='chat-avatar avatar-tiny' src={message.avatar} />
+                <img className="chat-avatar avatar-tiny" src={message.avatar} />
               </div>
             )
           }
 
           return (
-            <div key={index} className='chat-other'>
+            <div key={index} className="chat-other">
               <Link to={`/profile/${message.username}`}>
-                <img className='avatar-tiny' src={message.avatar} />
+                <img className="avatar-tiny" src={message.avatar} />
               </Link>
-              <div className='chat-message'>
-                <div className='chat-message-inner'>
+              <div className="chat-message">
+                <div className="chat-message-inner">
                   <Link to={`/profile/${message.username}`}>
                     <strong>{message.username}: </strong>
                   </Link>
@@ -111,21 +95,8 @@ function Chat() {
           )
         })}
       </div>
-      <form
-        onSubmit={handleSubmit}
-        id='chatForm'
-        className='chat-form border-top'
-      >
-        <input
-          value={state.fieldValue}
-          onChange={handleFieldChange}
-          ref={chatField}
-          type='text'
-          className='chat-field'
-          id='chatField'
-          placeholder='Type a message…'
-          autoComplete='off'
-        />
+      <form onSubmit={handleSubmit} id="chatForm" className="chat-form border-top">
+        <input value={state.fieldValue} onChange={handleFieldChange} ref={chatField} type="text" className="chat-field" id="chatField" placeholder="Type a message…" autoComplete="off" />
       </form>
     </div>
   )
